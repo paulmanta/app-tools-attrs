30e5bfc7563bfce1e0b72b8d71275a3d6208f55e
Paul Manta <paulc.mnt@gmail.com>
2018-09-02 00:58:31 +0200

    Simplify checks and reduce number of lines

--- library/src/main/java/com/github/paulmanta/apptoolsattrs/AppToolsAttrs.java
+++ library/src/main/java/com/github/paulmanta/apptoolsattrs/AppToolsAttrs.java
@@ -13,101 +13,62 @@ import android.support.annotation.StyleableRes;
 public abstract class AppToolsAttrs {
 
     @Nullable
-    public static CharSequence getText(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getText(appToolsAttr);
-        }
-        return a.getText(appAttr);
+    public static CharSequence getText(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr) {
+        return a.getText(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr);
     }
 
     @Nullable
-    public static String getString(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getString(appToolsAttr);
-        }
-        return a.getString(appAttr);
+    public static String getString(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr) {
+        return a.getString(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr);
     }
 
-    public static boolean getBoolean(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, boolean def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getBoolean(appToolsAttr, def);
-        }
-        return a.getBoolean(appAttr, def);
+    public static boolean getBoolean(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, boolean defVal) {
+        return a.getBoolean(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static int getInt(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getInt(appToolsAttr, def);
-        }
-        return a.getInt(appAttr, def);
+    public static int getInt(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, int defVal) {
+        return a.getInt(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static float getFloat(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, float def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getFloat(appToolsAttr, def);
-        }
-        return a.getFloat(appAttr, def);
+    public static float getFloat(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, float defVal) {
+        return a.getFloat(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
     @ColorInt
-    public static int getColor(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, @ColorInt int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getColor(appToolsAttr, def);
-        }
-        return a.getColor(appAttr, def);
+    public static int getColor(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, @ColorInt int defVal) {
+        return a.getColor(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static int getInteger(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getInteger(appToolsAttr, def);
-        }
-        return a.getInteger(appAttr, def);
+    public static int getInteger(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, int defVal) {
+        return a.getInteger(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static float getDimension(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, float def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getDimension(appToolsAttr, def);
-        }
-        return a.getDimension(appAttr, def);
+    public static float getDimension(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, float defVal) {
+        return a.getDimension(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static int getDimensionPixelOffset(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getDimensionPixelOffset(appToolsAttr, def);
-        }
-        return a.getDimensionPixelOffset(appAttr, def);
+    public static int getDimensionPixelOffset(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, int defVal) {
+        return a.getDimensionPixelOffset(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
-    public static int getDimensionPixelSize(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getDimensionPixelSize(appToolsAttr, def);
-        }
-        return a.getDimensionPixelSize(appAttr, def);
+    public static int getDimensionPixelSize(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, int defVal) {
+        return a.getDimensionPixelSize(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
     @AnyRes
-    public static int getResourceId(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr, @AnyRes int def) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getResourceId(appToolsAttr, def);
-        }
-        return a.getResourceId(appAttr, def);
+    public static int getResourceId(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr, @AnyRes int defVal) {
+        return a.getResourceId(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr, defVal);
     }
 
     @Nullable
-    public static Drawable getDrawable(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getDrawable(appToolsAttr);
-        }
-        return a.getDrawable(appAttr);
+    public static Drawable getDrawable(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr) {
+        return a.getDrawable(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr);
     }
 
     @RequiresApi(26)
     @Nullable
-    public static Typeface getFont(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int appToolsAttr) {
-        if (isInEditMode && appToolsAttr != 0 && a.hasValue(appToolsAttr)) {
-            return a.getFont(appToolsAttr);
-        }
-        return a.getFont(appAttr);
+    public static Typeface getFont(@NonNull TypedArray a, boolean isInEditMode, @StyleableRes int appAttr, @StyleableRes int toolsAttr) {
+        return a.getFont(isInEditMode && a.hasValue(toolsAttr) ? toolsAttr : appAttr);
     }
 
     private AppToolsAttrs() {
